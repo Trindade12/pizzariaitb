3f79845f386fe12d3f115e1971ed54397ed05acb
Trindade12 <rm91825@estudante.fieb.edu.br>
2024-06-26 10:56:32 -0300

    Listagem

---
+++ src/pages/Listagem.jsx
@@ -0,0 +1,11 @@
+import React, {useState, useEffet} from 'react'
+
+const Listagem = () => {
+    return (
+        <>
+            <h2>Pizzas ITB</h2>
+        </>
+    )
+}
+
+export default Listagem
